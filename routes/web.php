6198f970baa01625d31c2cdd707cdee01536020d
samulhasan <ihtisamul.hasan@bmkg.go.id>
2024-08-21 12:57:43 +0900

    push

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/display', function () {
+    return view('display');
+});
+
+
+
+Route::get('/aws', function () {
+    return view('aws');
+});
+
